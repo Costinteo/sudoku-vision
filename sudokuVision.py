75e76542e7b42ede89a603a563342ce21accd10b
Costinteo <costinteo@gmail.com>
2021-11-30 02:57:32 +0200

    after hours of searching / trying alternative algos, I still gotta do BFS

--- sudokuVision.py
+++ sudokuVision.py
@@ -13,6 +13,7 @@ NUMBER_CELL_MIN_MEAN = 15
 VERBOSE = False
 CLASSIC = "classic"
 JIGSAW = "jigsaw"
+MODE = JIGSAW
 
 def printInfo(infoString):
     if not VERBOSE:
@@ -140,6 +141,148 @@ def extractCells(img):
             cells[i][j] = cell
     return cells
 
+def writeSolution(cells, outputPath, components = None):
+    outputFile = open(outputPath, "w")
+    predictedValues = [["" for _ in range(9)] for _ in range(9) ]
+    # iterate through the cells
+    # write their values to corresponding output file
+    for i in range(9):
+        for j in range(9):
+            charToWrite = "o" if isCellEmpty(cells[i][j]) else "x"
+            if type(components) is np.ndarray:
+                charToWrite = str(components[i][j]) + charToWrite
+            outputFile.write(charToWrite)
+            predictedValues[i][j] = charToWrite
+        if i != 8:
+            outputFile.write("\n")
+    return predictedValues
+
+def isNewComponent(cell, y, x):
+    """
+    Decides if cell passed as arg is a part of a new component or not
+
+    Parameters:
+    cell: the cell in question
+       y: the y position in the matrix
+       x: the x position in the matrix
+
+    Returns:
+    int: 0 if it's a new component
+         1 if it's a part of the left one
+         2 if it's a part of the above one
+         3 if it's a part of both left and above
+    """
+
+    cellHeight, cellWidth = np.shape(cell)
+
+    leftSideSum = 0
+    topSideSum = 0
+
+    noLeft = False
+    noTop = False
+    
+    # the thick contours will remain in the cells once extracted
+    # we can use this to determine the thick contours positions relative to the cell
+    # we sum up the pixels that are a part of the contour to determine if there's a contour
+    # then we use this to determine if the cell is part of a new component or not
+    for k in range(cellHeight):
+        leftSideSum += np.sum(cell[k][:cellWidth // 7]) // 255
+
+    for k in range(cellHeight // 7):
+        topSideSum += np.sum(cell[k]) // 255
+
+    if leftSideSum < cellHeight and x > 0:
+        noLeft = True
+
+    if topSideSum < cellWidth and y > 0:
+        noTop = True
+
+    if noLeft and noTop:
+        return 3
+    elif noLeft:
+        return 1
+    elif noTop:
+        return 2
+
+    return 0
+    
+
+def markComponents(contourCells):
+    """
+    Generates a matrix of what component each cell is a part of
+
+    Parameters:
+    contourCells: matrix of the cells with only the highlighted thick contour
+
+    Returns:
+    np.array: matrix where each cell is marked with a component number
+    """
+    
+    # this doesn't work!!!
+    # I need LEE to do it
+    # FUCK THIS
+    # to do:
+    # lee using god damn code matrix
+
+    # current component number
+    compNumber = 1
+    components = np.zeros((9, 9), np.uint8) 
+    showImg(255 - contourCells[5][8])
+    for i in range(9):
+        print()
+        for j in range(9):
+            code = isNewComponent(contourCells[i][j], i, j)
+            print(code, end = " ")
+            # new component
+            if code == 0:
+                components[i][j] = compNumber
+                compNumber += 1
+            # component same as left
+            elif code == 1:
+                components[i][j] = components[i][j-1]
+            # component same as above
+            elif code == 2:
+                components[i][j] = components[i-1][j]
+            # component is interior or in need of flood
+            elif code == 3:
+                # first we check if flood is needed
+                # by verifying if the leftside cell is different from topside component
+                if components[i][j-1] != components[i-1][j]:
+                    # we flood to overwrite any cells we mistook
+                    # for a new component
+                    newCompValue = min(components[i-1][j], components[i][j-1])
+                    compToFlood = max(components[i-1][j], components[i][j-1])
+    
+                
+                    if components[i-1][j] > components[i][j-1]:
+                        # flood upwards only, special case
+                        for k in range(i - 1, -1, -1):
+                            if components[k][j] != compToFlood:
+                                break
+                            components[k][j] = newCompValue
+                    else:
+                        # start flooding horizontally
+                        for k in range(j - 1, -1, -1):
+                            # start flooding vertically
+                            for l in range(i - 1, -1, -1):
+                                # if we reached vertical end of current component
+                                if components[l][k] != compToFlood:
+                                    break
+                                components[l][k] = newCompValue
+                            # if we reached horizontal end of current component
+                            if components[i][k] != compToFlood:
+                                #print(i, k, compToFlood, newCompValue)
+                                break
+                            components[i][k] = newCompValue
+                    # if we flooded the last component used
+                    # we need to decrement compNumber
+                    compNumber -= 1
+                # we take the above component in all cases
+                components[i][j] = components[i-1][j]
+            #showImg(255 - contourCells[i][j])
+    print()
+    return components
+
 def isCorrectlyPredicted(predictedCells, truthPath):
     """
     Checks predicted solution against real solution.
@@ -152,17 +295,27 @@ def isCorrectlyPredicted(predictedCells, truthPath):
     bool: True if correct, False if incorrect
     """
     try:
-        truthCells = open(truthPath, "r").readlines()
+        truthFile = open(truthPath, "r")
     except:
         print("Ground truth path incorrect!")
         exit(1)
-    for i in range(len(predictedCells)):
-        for j in range(len(predictedCells[i])):
-            if predictedCells[i][j] != ord(truthCells[i][j]):
+
+    truthCells = [["" for _ in range(9)] for _ in range(9)]
+    if MODE == CLASSIC:
+        truthCells = truthFile.readlines()   
+    elif MODE == JIGSAW:
+        for i, line in enumerate(truthFile.readlines()):
+            for j in range(9):
+                truthCells[i][j] = line[j*2] + line[j*2+1]
+
+
+    for i in range(9):
+        for j in range(9):
+            if predictedCells[i][j] != truthCells[i][j]:
                 if VERBOSE:
                     print(f"Solution failed at {i}, {j}")
-                    print(f"Predicted: {chr(predictedCells[i][j])}, {predictedCells[i][j]}")
-                    print(f"Truth: {truthCells[i][j]}, {ord(truthCells[i][j])}")
+                    print(f"Predicted: {predictedCells[i][j]}")
+                    print(f"Truth: {truthCells[i][j]}")
                 return False
     return True
 
@@ -172,7 +325,7 @@ def showImg(img, title="default", timer=0):
     cv.waitKey(timer)
     cv.destroyAllWindows()
 
-def sudokuVision(inputPath, outputPath, truthPath=None, mode=CLASSIC):
+def sudokuVision(inputPath, outputPath, truthPath=None):
     print(f"Running {inputPath} through Sudoku Vision...")
     
 
@@ -237,43 +390,39 @@ def sudokuVision(inputPath, outputPath, truthPath=None, mode=CLASSIC):
     #showImg(255 -warpedImg)
 
     printInfo("Extracting cells...")
+    
     # extract the cells
     cells = extractCells(warpedImg)
-    
-    mode = JIGSAW
-    if mode != CLASSIC:
-
+    # if mode is JIGSAW we also want to extract the cells
+    # after highlighting the components contour
+    eroded = None
+    if MODE == JIGSAW:
         dilated = cv.dilate(warpedImg, np.ones((2, 2), np.uint8))
         kernel = np.ones((6, 1), np.uint8)
         # erode vertical lines then horizontal lines
         eroded = cv.erode(dilated, kernel)
         eroded = cv.erode(eroded, np.transpose(kernel))
+        # extract the contour cells
+        contourCells = extractCells(eroded)
+        # based on the contour cells, generate the matrix with components
+        componentMatrix = markComponents(contourCells)
+        print(componentMatrix)
+        showImg(eroded)
 
-        
-
-        return
     printInfo(f"Writing to file {outputPath}...")
-    outputFile = open(outputPath, "w")
-    predictedValues = np.empty((9, 9), np.uint8)
-    # iterate through the cells
-    # write their values to corresponding output file
-    for i in range(len(cells)):
-        for j in range(len(cells[i])):
-            outputFile.write("o" if isCellEmpty(cells[i][j]) else "x")
-            predictedValues[i][j] = ord("o") if isCellEmpty(cells[i][j]) else ord("x")
-            #showImg(255 - cells[i][j])
-        outputFile.write("\n")
+    predictedValues = writeSolution(cells, outputPath, componentMatrix if MODE != CLASSIC else None)
     
     if truthPath:
         print("Correct!" if isCorrectlyPredicted(predictedValues, truthPath) else "Incorrect!")
+    
     printInfo("Done!")
 
 if __name__ == "__main__":
     paths = sys.argv[1:]
     #VERBOSE = True
-    
+    MODE = JIGSAW
     for path in paths:
         # extract filename (using os.sep so it works on any platform)
         filename = path[path.rfind(os.sep) + 1 : path.rfind(".")]
         cwd = path[:path.rfind(os.sep)]
-        sudokuVision(path, f".{os.sep}output{os.sep}{filename}_predicted.txt", f"{cwd}{os.sep}{filename}_gt1.txt")
+        sudokuVision(path, f".{os.sep}output{os.sep}{filename}_predicted.txt", f"{cwd}{os.sep}{filename}_gt.txt")
